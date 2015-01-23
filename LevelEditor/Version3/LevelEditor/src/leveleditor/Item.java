236cf937e0e0c29be83dc2c0611919327109d9a8
Justin Stribling <jastribl@uwaterloo.ca>
2015-01-23 01:43:33 -0500

    Version3

---
+++ LevelEditor/Version3/LevelEditor/src/leveleditor/Item.java
@@ -0,0 +1,64 @@
+package leveleditor;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Item implements Comparable, Cloneable {
+
+    private int x, y;
+
+    public Item(int xG, int yG) {
+        x = xG;
+        y = yG;
+    }
+
+    @Override
+    protected Item clone() throws CloneNotSupportedException {
+        return (Item) super.clone();
+    }
+
+    public final int getX() {
+        return x;
+    }
+
+    public final int getY() {
+        return y;
+    }
+
+    public final void setLocation(int xG, int yG) {
+        x = xG;
+        y = yG;
+    }
+
+    public final void draw(Graphics g, boolean filled, int minX, int minY, int scale, int angle) {
+        int height = (int) (angle / 90.0 * scale);
+        int depth = scale - height;
+        g.setColor(Color.white);
+        g.drawRect((x * scale) + minX, (y * height) + minY, scale, height);
+        g.drawRect((x * scale) + minX, (y * height) + minY + height, scale, depth);
+        if (filled) {
+            g.setColor(Color.green);
+            g.fillRect((x * scale) + minX + 1, (y * height) + minY + 1, scale - 1, height - 1);
+            g.setColor(Color.blue);
+            g.fillRect((x * scale) + minX + 1, (y * height) + minY + height + 1, scale - 1, depth - 1);
+        } else {
+            g.setColor(Color.gray);
+            g.drawRect((x * scale) + minX + 1, (y * height) + minY + 1, scale - 1, height - 1);
+            g.drawRect((x * scale) + minX + 1, (y * height) + minY + height + 1, scale - 1, depth - 1);
+        }
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Item i2 = (Item) o;
+        if (this.getX() == i2.getX()) {
+            if (this.getY() == i2.getY()) {
+                return 0;
+            } else {
+                return this.getY() > i2.getY() ? 1 : -1;
+            }
+        } else {
+            return this.getX() > i2.getX() ? 1 : -1;
+        }
+    }
+}
---
+++ LevelEditor/Version3/LevelEditor/src/leveleditor/ObjectMenu.java
@@ -0,0 +1,45 @@
+package leveleditor;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+public class ObjectMenu {
+
+    private final int items = 1;
+    private final Item[] objects = new Item[items];
+    private final int scale, angle;
+
+    public ObjectMenu(int scaleG, int angleG) {
+        scale = scaleG;
+        angle = angleG;
+        for (int i = 0; i < items; i++) {
+            objects[i] = new Item(scale, angle);
+            objects[i].setLocation(45, 45);
+        }
+    }
+
+    public final void zoomTo(int angleG) {
+//        for (Item object : objects) {
+//            object.tiltTo(angleG);
+//        }
+    }
+
+    public Item getSelectedMenuItem(Point testLocation) throws CloneNotSupportedException {
+        Item object;
+        for (int i = 0; i < items; i++) {
+            object = objects[i];
+            Rectangle rectangle = new Rectangle(object.getX(), object.getY(), 64, 64);
+            if (rectangle.contains(testLocation)) {
+                return (Item) object.clone();
+            }
+        }
+        return null;
+    }
+
+    public final void draw(Graphics g) {
+        for (int i = 0; i < items; i++) {
+            objects[i].draw(g, true, 0, 0, scale, angle);
+        }
+    }
+}
