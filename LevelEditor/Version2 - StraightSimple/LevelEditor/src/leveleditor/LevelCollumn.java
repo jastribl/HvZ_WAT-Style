7523288edae081dd905eb6f671249fe177b17225
Justin Stribling <jastribl@uwaterloo.ca>
2015-02-11 20:57:02 -0500

    Old Version Cleanup

--- LevelEditor/Version2 - StraightSimple/LevelEditor/src/leveleditor/LevelCollumn.java
+++
@@ -1,6 +0,0 @@
-package leveleditor;
-
-import java.util.ArrayList;
-
-public class LevelCollumn extends ArrayList<LevelObject> {
-}
--- LevelEditor/Version2 - StraightSimple/LevelEditor/src/leveleditor/LevelRows.java
+++
@@ -1,6 +0,0 @@
-package leveleditor;
-
-import java.util.ArrayList;
-
-public class LevelRows extends ArrayList<LevelObject> {
-}
