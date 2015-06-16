924ee77a93f6a47106212f4c33d948c98c3793ae
Justin Stribling <jastribl@uwaterloo.ca>
2015-06-16 18:59:49 -0400

    cleanup

--- HvZ-TheGame/HvZ-TheGame/Constants.h
+++ HvZ-TheGame/HvZ-TheGame/Constants.h
@@ -1,54 +1,58 @@
 #pragma once
 
-//File contains only static constant variables and methods
-
-static int SCREEN_SIZE_X = 1920;
-static int SCREEN_SIZE_Y = 1080;
+static const int SCREEN_SIZE_X = 1920;
+static const int SCREEN_SIZE_Y = 1080;
 
 //Sidebar
-static int SIDEBAR_ORIGIN_X = 1620;
-static int SIDEBAR_ORIGIN_Y = 0;
-static int SIDEBAR_LENGTH = SCREEN_SIZE_X - SIDEBAR_ORIGIN_X;
+static const int SIDEBAR_ORIGIN_X = 1620;
+static const int SIDEBAR_ORIGIN_Y = 0;
+static const int SIDEBAR_LENGTH = SCREEN_SIZE_X - SIDEBAR_ORIGIN_X;
 
 //HP and MP Bar
-static int HP_BORDER = 3;
-static int HP_LENGTH = 299;
-static int HP_WIDTH = 150;
-static int HP_ORIGIN_X = SCREEN_SIZE_X - HP_LENGTH;
-static int HP_ORIGIN_Y = SCREEN_SIZE_Y - HP_WIDTH;
-static int HP_TEXT_X = 20;
-static int HP_TEXT_Y = 15;
-static int HP_MPTEXT_X = 20;
-static int HP_MPTEXT_Y = 70;
-static int HP_BAR_X = 20;
-static int HP_BAR_Y = 45;
-static int HP_BAR_LENGTH = 260;
-static int HP_BAR_WIDTH = 20;
-static int HP_MPBAR_X = 20;
-static int HP_MPBAR_Y = 100;
+static const int HP_BORDER = 3;
+static const int HP_LENGTH = 299;
+static const int HP_WIDTH = 150;
+static const int HP_ORIGIN_X = SCREEN_SIZE_X - HP_LENGTH;
+static const int HP_ORIGIN_Y = SCREEN_SIZE_Y - HP_WIDTH;
+static const int HP_TEXT_X = 20;
+static const int HP_TEXT_Y = 15;
+static const int HP_MPTEXT_X = 20;
+static const int HP_MPTEXT_Y = 70;
+static const int HP_BAR_X = 20;
+static const int HP_BAR_Y = 45;
+static const int HP_BAR_LENGTH = 260;
+static const int HP_BAR_WIDTH = 20;
+static const int HP_MPBAR_X = 20;
+static const int HP_MPBAR_Y = 100;
 
 //Inventory Boxes
-static int BOX_LENGTH = 73;
-static int BOX_WIDTH = 73;
-static int BOX_BORDER = 3;
-static int BOX_LGBORDER = 2;
-static int BOX_MOVEMENT_X = 70;
-static int BOX_MOVEMENT_Y = 70;
-static int BOX_PERROW = 4;
-static int BOX_NUM = 28;
-static int BOX_SIZE_LENGTH = BOX_PERROW*BOX_LENGTH - BOX_BORDER*(BOX_PERROW - 1);
-static int BOX_SIZE_WIDTH = ((BOX_NUM - 1) / BOX_PERROW + 1)*BOX_WIDTH - BOX_BORDER*((BOX_NUM - 1) / BOX_PERROW);
-static int BOX_ORIGIN_X = SIDEBAR_ORIGIN_X + BOX_LGBORDER + 6;
-static int BOX_ORIGIN_Y = 425 + BOX_LGBORDER;
+static const int BOX_LENGTH = 73;
+static const int BOX_WIDTH = 73;
+static const int BOX_BORDER = 3;
+static const int BOX_LGBORDER = 2;
+static const int BOX_MOVEMENT_X = 70;
+static const int BOX_MOVEMENT_Y = 70;
+static const int BOX_PERROW = 4;
+static const int BOX_NUM = 28;
+static const int BOX_SIZE_LENGTH = BOX_PERROW*BOX_LENGTH - BOX_BORDER*(BOX_PERROW - 1);
+static const int BOX_SIZE_WIDTH = ((BOX_NUM - 1) / BOX_PERROW + 1)*BOX_WIDTH - BOX_BORDER*((BOX_NUM - 1) / BOX_PERROW);
+static const int BOX_ORIGIN_X = SIDEBAR_ORIGIN_X + BOX_LGBORDER + 6;
+static const int BOX_ORIGIN_Y = 425 + BOX_LGBORDER;
 
 //World blocks
-static int BLOCK_SIZE = 64;
-static int HALF_BLOCK_SIZE = BLOCK_SIZE / 2;
+static const int BLOCK_SIZE = 64;
+static const int HALF_BLOCK_SIZE = BLOCK_SIZE / 2;
 
-static int CHARACTER_WIDTH = BLOCK_SIZE, CHARACTER_HEIGHT = BLOCK_SIZE * 2;
+//Character
+static const int CHARACTER_WIDTH = BLOCK_SIZE, CHARACTER_HEIGHT = BLOCK_SIZE * 2;
 
-enum ITEM_GROUP { BLOCK, SPECIAL, CHARACTER };
+//movement settings
+static const int MAX_MOVEMENT_CHECK_THRESHOLD = 10;
 
+//Item groups
+static const enum ITEM_GROUP { BLOCK, SPECIAL, CHARACTER };
+
+//common algorithms
 static sf::Vector3i& cartesianToIsometric(const int x, const int y, const int z) {
 	return sf::Vector3i(x - y, ((x + y) / 2) - z, z);
 }
@@ -67,12 +71,12 @@ static sf::Vector3i& screenToGrid(const sf::Vector2f& p, const int z) {
 }
 
 template <typename  T>
-bool VectorsAreEqual(T const& a, T const& b) {
+static bool VectorsAreEqual(T const& a, T const& b) {
 	return (a.x == b.x && a.y == b.y && a.z == b.z);
 }
 
 template <typename T>
-void PrintVector(T const& v) {
+static void PrintVector(T const& v) {
 	std::cout << "(" << v.x << "," << v.y << ","v.z")" << std << enbdl;
 }
 
